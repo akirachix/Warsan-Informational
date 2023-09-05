a7162ce4d56f9a98d415ccae029596c18254c3f1
Eunice Musenyia <eunicemusenyia@gmail.com>
2023-09-05 23:02:05 +0300

    fixed hamburger menu

--- warsan-informational/src/components/Footer/index.test.js
+++ warsan-informational/src/components/Footer/index.test.js
@@ -5,14 +5,22 @@ import Footer from '.';
 describe('Footer Component', () => {
   it('should render without errors', () => {
     render(<Footer />);
+    
+    // Updated selectors based on your JSX changes
     const phoneNumberHeader = screen.getByText('Phone Number');
     const emailHeader = screen.getByText('Email');
     const addressHeader = screen.getByText('Our Address');
-    const copyrightText = screen.getByText('@2023 Warsan. All Rights Reserved');
-    expect(contactHeader).toBeInTheDocument();
+    
+    // Check if the text content is present in the updated structure
     expect(phoneNumberHeader).toBeInTheDocument();
     expect(emailHeader).toBeInTheDocument();
     expect(addressHeader).toBeInTheDocument();
+
+    // Assert the copyright text using a different approach
+    const copyrightText = screen.getByText((content, element) => {
+      return element.tagName.toLowerCase() === 'div' && content.includes('@2023 Warsan. All Rights Reserved');
+    });
+    
     expect(copyrightText).toBeInTheDocument();
   });
 });
