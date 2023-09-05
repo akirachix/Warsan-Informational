35d6d20f056212e86e949b03c522e8afad77ca85
Eunice Musenyia <eunicemusenyia@gmail.com>
2023-09-05 14:43:01 +0300

    added const layout

--- warsan-informational/src/App.js
+++ warsan-informational/src/App.js
@@ -1,7 +1,7 @@
 // Layout.js
 import React from 'react';
 import Navbar from './components/navbar'; 
-import Footer from './components/Footer';
+import Footer from './components/footer';
 
 const Layout = ({ children }) => {
   return (
