392998901cf0b6d9eab7cc928441da38cb6d0a6e
kayitaclever <kayitaramirwaaclever>
2023-09-06 18:57:12 +0300

    updated test.js

--- warsan-informational/src/components/Team/index.jsx
+++ warsan-informational/src/components/Team/index.jsx
@@ -1,32 +1,53 @@
 import React from "react";
-import './style.css'
-const Team = ()=>{
-    return (
-        <>
-        <h1>OUR TEAM</h1><br></br>
-        <div className="team-container">
-          <div className="team-member" data-testid = "Aniso" >
-            <img src={process.env.PUBLIC_URL + '/images/aniso.jpg'} alt="Aniso" />
-            <p>Aniso Mohamed Kilas <br></br>Frontend Developer</p>
+import './style.css';
+
+const teamMembers = [
+  {
+    id: "Aniso",
+    name: "Aniso Mohamed Kilas",
+    role: "Frontend Developer",
+    imageSrc: '/images/aniso.jpg',
+  },
+  {
+    id: "Eunice",
+    name: "Eunice Musenyia",
+    role: "Full Stack Developer",
+    imageSrc: '/images/eunice .jpg',
+  },
+  {
+    id: "Susan",
+    name: "Susan Maina",
+    role: "Software Developer",
+    imageSrc: '/images/susan.jpg',
+  },
+  {
+    id: "Clever",
+    name: "Clever Kayitaramirwa",
+    role: "Full Stack Developer",
+    imageSrc: '/images/clever.jpg',
+  },
+  {
+    id: "Joy",
+    name: "Joy Mitingi",
+    role: "Mobile Developer",
+    imageSrc: '/images/joy.jpg',
+  },
+];
+
+const Team = () => {
+  return (
+    <>
+      <h1>OUR TEAM</h1><br></br>
+      <div className="team-container">
+        {teamMembers.map((member) => (
+          <div className="team-member" key={member.id} data-testid={member.id}>
+            <img src={member.imageSrc} alt={member.id} />
+            <p>{member.name}<br></br>{member.role}</p>
           </div>
-          <div className="team-member" data-testid = "Eunice">
-            <img src={process.env.PUBLIC_URL + '/images/eunice .jpg'} alt="Eunice" />
-            <p>Eunice Musenyia <br></br> Full Stack Developer</p>
-          </div>
-          <div className="team-member" data-testid = "Susan">
-            <img src={process.env.PUBLIC_URL + '/images/susan.jpg'} alt="Susan" />
-            <p>Susan Maina <br></br>Software Developer</p>
-          </div>
-          <div className="team-member" data-testid = "Clever">
-            <img src={process.env.PUBLIC_URL + '/images/clever.jpg'} alt="Clever" />
-            <p>Clever Kayitaramirwa<br></br>Full Stack Developer</p>
-          </div>
-          <div className="team-member" data-testid = "Joy">
-            <img src={process.env.PUBLIC_URL + '/images/joy.jpg'} alt="Joy" />
-            <p>Joy Mitingi<br></br>Mobile Developer</p>
-          </div>
-        </div>
-        </>
-      );
-    }
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Team;
