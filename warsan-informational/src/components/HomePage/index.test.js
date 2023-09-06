445dc26df4ad8261b7204e3659c314deff96e5e9
kilas-Aniso <kilasaniso@gmail.com>
2023-09-06 20:50:58 +0300

    added more testing

--- warsan-informational/src/components/HomePage/index.test.js
+++ warsan-informational/src/components/HomePage/index.test.js
@@ -3,7 +3,7 @@ import { render, screen, act } from '@testing-library/react';
 import HomePage from './index.jsx';
 import 'jest-styled-components';
 
-jest.useFakeTimers(); 
+jest.useFakeTimers(); // Enable fake timers
 
 describe('HomePage Component', () => {
   it('should toggle between text options in the slideshow', async () => {
@@ -11,15 +11,16 @@ describe('HomePage Component', () => {
     const slideshowText = screen.getByTestId('slideshow-text');
     const learnMoreButton = screen.getByText('Learn More');
 
+    // Wait for the initial text to display
+    await screen.findByText('Save a child');
+
+    // Check that the initial text is displayed
+    expect(slideshowText.textContent).toBe('Save a child');
 
     // Trigger the interval again
     act(() => {
       jest.advanceTimersByTime(2500);
     });
-
-    // Check that the initial text is displayed
-    expect(slideshowText.textContent).toBe('Save a child');
-
 
     // Check that the text has switched to the second option
     expect(slideshowText.textContent).toBe('Save a nation');
@@ -28,40 +29,36 @@ describe('HomePage Component', () => {
     expect(learnMoreButton).toBeInTheDocument();
   });
 
-  it('should have correct background styling', () => {
+  it('should have correct styling', () => {
     render(<HomePage />);
     const homeSection = screen.getByTestId('home-section');
-
-
-
-  it('should have correct text styling', () => {
-    render(<HomePage />);
     const slideshowText = screen.getByTestId('slideshow-text');
-
-    const slideshowTextStyles = window.getComputedStyle(slideshowText);
-
-    // Check text styling
-    expect(slideshowTextStyles.getPropertyValue('font-weight')).toBe('bold');
-    expect(slideshowTextStyles.getPropertyValue('font-size')).toBe('2em');
-  });
-
-  it('should have correct button styling', () => {
-    render(<HomePage />);
     const learnMoreButton = screen.getByText('Learn More');
 
-    const learnMoreButtonStyles = window.getComputedStyle(learnMoreButton);
+    // Check background image and color
+    expect(homeSection).toHaveStyleRule('background-image', 'url(/public/images/childone.jpg)');
+    expect(homeSection).toHaveStyleRule('background-color', 'rgba(0, 0, 0, 0.8)');
+
+    // Check text styling
+    expect(slideshowText).toHaveStyle({
+      fontWeight: 'bold',
+      fontSize: '7rem',
+      color: '#FFFFFF',
+      marginTop: '-15%',
+    });
 
     // Check button styling
-    expect(learnMoreButtonStyles.getPropertyValue('width')).toBe('231.25px');
-    expect(learnMoreButtonStyles.getPropertyValue('height')).toBe('65px');
-    expect(learnMoreButtonStyles.getPropertyValue('background-color')).toBe('#1175D2');
-    expect(learnMoreButtonStyles.getPropertyValue('color')).toBe('#fff');
-    expect(learnMoreButtonStyles.getPropertyValue('border')).toBe('none');
-    expect(learnMoreButtonStyles.getPropertyValue('border-radius')).toBe('5px');
-    expect(learnMoreButtonStyles.getPropertyValue('justify-content')).toBe('center');
-    expect(learnMoreButtonStyles.getPropertyValue('align-items')).toBe('center');
-    expect(learnMoreButtonStyles.getPropertyValue('margin-top')).toBe('480px');
-    expect(learnMoreButtonStyles.getPropertyValue('cursor')).toBe('pointer');
+    expect(learnMoreButton).toHaveStyle({
+      width: '231.25px',
+      height: '65px',
+      backgroundColor: '#1175D2',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: '480px',
+      cursor: 'pointer',
+    });
   });
-})
 });
