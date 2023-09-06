1cca569b6c363a69f3857557ab583a69eaa48c6f
kayitaclever <kayitaramirwaaclever>
2023-09-06 10:48:53 +0300

    updated team test

--- warsan-informational/src/components/Team/index.test.js
+++ warsan-informational/src/components/Team/index.test.js
@@ -4,31 +4,37 @@ import Team from './index.jsx';
 describe('Team Component', () => {
   it('should render without errors', () => {
     render(<Team />);
-    // Check for the presence of the component title
+
+    // Testing presence of the component title
     const teamTitle = screen.getByText('OUR TEAM');
     expect(teamTitle).toBeInTheDocument();
-    // Check for each team member's image and name
+
+    // Testing each team member's image and name
     const teamMembers = screen.getAllByRole('img');
-    expect(teamMembers).toHaveLength(5); // Assuming you have 5 team members
-    // Check for specific team member details
+    expect(teamMembers).toHaveLength(5); 
+
+    //Testing specific team member details
     const teamMember1 = screen.getByTestId('Aniso');
     const teamMember2 = screen.getByTestId('Eunice');
     const teamMember3 = screen.getByTestId('Susan');
     const teamMember4 = screen.getByTestId('Clever');
     const teamMember5 = screen.getByTestId('Joy');
-    // Check that each team member's image and name are present
+
+    // Testing presence team member's image and name
     expect(teamMember1).toBeInTheDocument();
     expect(teamMember2).toBeInTheDocument();
     expect(teamMember3).toBeInTheDocument();
     expect(teamMember4).toBeInTheDocument();
     expect(teamMember5).toBeInTheDocument();
-    // You can also check for specific team member images by alt text
+
+    // Testing for specific team member images by alt text
     const teamMember1Image = screen.getByAltText('Aniso');
     const teamMember2Image = screen.getByAltText('Eunice');
     const teamMember3Image = screen.getByAltText('Susan');
     const teamMember4Image = screen.getByAltText('Clever');
     const teamMember5Image = screen.getByAltText('Joy');
-    // Check that each team member's image is present
+
+    // Testing each team member's image 
     expect(teamMember1Image).toBeInTheDocument();
     expect(teamMember2Image).toBeInTheDocument();
     expect(teamMember3Image).toBeInTheDocument();
