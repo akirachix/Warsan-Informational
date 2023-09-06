3e15974f41809395a6272a8a8ad89335ab9458c5
Eunice Musenyia <eunicemusenyia@gmail.com>
2023-09-06 21:59:38 +0300

    added more tests

--- warsan-informational/src/components/Footer/index.test.js
+++ warsan-informational/src/components/Footer/index.test.js
@@ -6,17 +6,16 @@ describe('Footer Component', () => {
   it('should render without errors', () => {
     render(<Footer />);
     
-    // Updated selectors based on your JSX changes
+    //Test footer contains presence
     const phoneNumberHeader = screen.getByText('Phone Number');
     const emailHeader = screen.getByText('Email');
     const addressHeader = screen.getByText('Our Address');
-    
-    // Check if the text content is present in the updated structure
+   
     expect(phoneNumberHeader).toBeInTheDocument();
     expect(emailHeader).toBeInTheDocument();
     expect(addressHeader).toBeInTheDocument();
 
-    // Assert the copyright text using a different approach
+    // Testing copyright text
     const copyrightText = screen.getByText((content, element) => {
       return element.tagName.toLowerCase() === 'div' && content.includes('@2023 Warsan. All Rights Reserved');
     });
